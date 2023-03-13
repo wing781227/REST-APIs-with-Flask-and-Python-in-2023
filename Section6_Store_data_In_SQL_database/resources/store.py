08c094793f1c02a252592c699a43b7c69d02ef38
Hsiu-Yuan Fan <wing781227@gmail.com>
2023-03-13 10:44:04 -0400

    81. Deleting related models with cascades

--- Section6_Store_data_In_SQL_database/resources/store.py
+++ Section6_Store_data_In_SQL_database/resources/store.py
@@ -1,23 +1,33 @@
+
+from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
-
 from db import db
 from models import StoreModel
 from schemas import StoreSchema
 
 
-blp = Blueprint("Stores", "stores", description="Operations on stores")
+blp = Blueprint("stores", __name__, description="Operations on stores")
 
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
+        # try:
+        #     return stores[store_id]
+        # except KeyError:
+        #     abort(404, message="Store not found.")
         store = StoreModel.query.get_or_404(store_id)
         return store
 
     def delete(self, store_id):
+        # try:
+        #     del stores[store_id]
+        #     return {"message": "Store deleted."}
+        # except KeyError:
+        #     abort(404, message="Store not found.")
         store = StoreModel.query.get_or_404(store_id)
         db.session.delete(store)
         db.session.commit()
@@ -28,11 +38,25 @@ class Store(MethodView):
 class StoreList(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
+        # return {"stores": list(stores.values())}
+        # return StoreModel.values()
         return StoreModel.query.all()
-
+    
     @blp.arguments(StoreSchema)
-    @blp.response(201, StoreSchema)
+    @blp.response(200, StoreSchema)
     def post(self, store_data):
+        # store_data = request.get_json()
+        # if "name" not in store_data:
+        #     abort(400, message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.")
+        
+        # for store in stores.values():
+        #     if (store_data["name"] == store["name"]):
+        #         abort(400, message=f"Item already exists.")
+        
+        # store_id = uuid.uuid4().hex
+        # store = {**store_data, "id": store_id}
+        # stores[store_id] = store
+        # return store, 201
         store = StoreModel(**store_data)
         try:
             db.session.add(store)
