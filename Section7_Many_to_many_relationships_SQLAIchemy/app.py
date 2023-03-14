44cdd5b0067aeadb8f27b807acafae9285fd6614
Hsiu-Yuan Fan <wing781227@gmail.com>
2023-03-13 23:29:52 -0400

    85. Many-to-many relationship between items and tags

---
+++ Section7_Many_to_many_relationships_SQLAIchemy/app.py
@@ -0,0 +1,187 @@
+# import uuid
+from flask import Flask#, request
+# from flask_smorest import abort
+# from db import items, stores
+
+from flask_smorest import Api
+
+from db import db
+import models
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
+
+def create_app(db_url=None):
+    app = Flask(__name__)
+
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    db.init_app(app)
+    api = Api(app)
+
+    with app.app_context():
+        db.create_all()
+
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
+    
+    return app
+
+# @app.get("/store")
+# def get_stores():
+#     return {"stores": stores}
+'''
+@app.get("/store") # http://127.00:500/store
+def get_stores():
+    return {"stores": list(stores.values())}'''
+
+# @app.post("/store")
+# def create_store():
+#     request_data = request.get_json()
+#     new_store = {"name": request_data["name"], "items": []}
+#     stores.append(new_store)
+#     return new_store, 201
+
+# @app.post("/store")
+# def create_store():
+#     store_data = request.get_json()
+#     store_id = uuid.uuid4().hex
+#     store = {**store_data, "id": store_id}
+#     stores[store_id] = store
+#     return store, 201
+'''@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.")
+    for store in stores.values():
+        if (store_data["name"] == store["name"]):
+            abort(400, message=f"Item already exists.")
+    
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201'''
+
+# @app.post("/store/<string:name>/item")
+# def create_item(name):
+#     request_data = request.get_json()
+#     for store in stores:
+#         if store["name"] == name:
+#             new_item = {"name": request_data["name"], "price": request_data["price"]}
+#             store["items"].append(new_item)
+#             return new_item, 201
+#     return {"message": "Store not found"}, 404
+
+# @app.post("/item")
+# def create_item():
+#     item_data = request.get_json()
+#     if item_data["store_id"] not in stores:
+#         return {"message": "Store not found"}, 404
+#         # abort(404, message="Store not found.")
+
+#     item_id = uuid.uuid4().hex
+#     item = {**item_data, "id": item_id}
+#     items[item_id] = item
+
+#     return item, 201
+
+'''@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    # Here not only we need to validate data exists,
+    # But also what type of data. Price should be a float,
+    # for example
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(400, message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.")
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+
+    if item_data["store_id"] not in stores:
+        abort(404, message="Store not found.")
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item, 201'''
+
+
+'''
+#
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")'''
+
+'''
+#
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.")
+
+    try:
+        item = items[item_id]
+        item |= item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")'''
+
+''' 
+# 
+@app.get("/item")
+def get_all_items():
+    # return "Hello, world! 123"
+    return {"items": list(items.values())}'''
+
+'''
+# @app.get("/store/<string:name>")
+# def get_store(name):
+#     for store in stores:
+#         if store["name"] == name:
+#             return store
+#     return {"message": "Store not found"}, 404
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        # return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")'''
+
+'''
+# @app.get("/store/<string:name>/item")
+# def get_item_in_store(name):
+#     for store in stores:
+#         if store["name"] == name:
+#             return {"items": store["items"]}
+#     return {"message": "Store not found"}, 404
+@app.get("/item/<string:item_id>")
+def get_item_in_store(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        # return {"message": "Item not found"}, 404
+        abort(404, message="Item not found."'''
---
+++ Section7_Many_to_many_relationships_SQLAIchemy/schemas.py
@@ -0,0 +1,40 @@
+from marshmallow import Schema, fields
+
+class PlainItemSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    price = fields.Float(required=True)
+
+class PlainStoreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    # name = fields.Str(required=True)
+    # price = fields.Str(required=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class ItemUpdateSchema(Schema):
+    name = fields.Str()
+    price = fields.Float()
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainStoreSchema()), dump_only=True)
+    # id = fields.Int(dump_only=True)
+    # name = fields.Str(required=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
